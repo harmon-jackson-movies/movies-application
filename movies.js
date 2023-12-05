2d3df9fa6d039c5f257c1205071cd9069a992603
Corey Harmon <harmon.coreyd@gmail.com>
2023-12-05 15:39:32 -0600

    added timer for loading image

--- movies.js
+++ movies.js
@@ -1,4 +1,10 @@
+
+// Imports getMoviesTitle function from API file and adding a timeout for loading image
 import {getMovieByTitle} from './movies-api.js'
 
 console.log(await getMovieByTitle(`Top+Gun`, "1986"));
+
 console.log(await getMovieByTitle(`Major+Payne`, "1995"));
+
+
+
